b73393366c8e776233a22ee0b18bbd8089bc01b1
Jose Sebastian <joesaby@gmail.com>
2025-05-08 17:16:38 +0100

    Add support for server rendering adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   output: 'server',
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
@@ -18,4 +21,6 @@ export default defineConfig({
       noExternal: ["node-domexception", "whatwg-url"],
     },
   },
+
+  adapter: netlify(),
 });
